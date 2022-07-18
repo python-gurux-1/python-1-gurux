6ceb144b3a4c7d1d48ad7de9ff71a183d41618c0
Asqarov-Fayzullo <86188397+Asqarov-Fayzullo@users.noreply.github.com>
2022-07-18 08:34:37 +0500

    Create json_py.py

---
+++ json_py.py
@@ -0,0 +1,23 @@
+#JSON FILEGA YOZISH
+
+# from data import maxsulotlar
+# import json
+
+# jsondata = json.dumps(maxsulotlar,indent=4)
+
+# file = open("data.json","w")
+
+# file.write(jsondata)
+
+# file.close()
+
+#JSON FILEDAN OLISH
+
+
+# import json
+
+# file = open("data.json","r")
+
+# data = json.loads(file.read())
+
+# print(data)
