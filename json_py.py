cdd9b0c30c52b47122dabe5c8da1510f7225e369
python-gurux-1 <108383232+python-gurux-1@users.noreply.github.com>
2022-07-18 07:50:56 +0300

    Update json_py.py

--- json_py.py
+++ json_py.py
@@ -21,3 +21,67 @@
 # data = json.loads(file.read())
 
 # print(data)
+"""
+print(
+    list(
+        filter(
+            lambda x:(not x.startswith("__")),
+            dir(data)
+            )
+        )
+    )
+
+"""
+import data
+import json
+
+with open("maxsulotlar.json","w") as maxsulotlar_json:
+    maxsulotlar_data = json.dumps(
+        data.maxsulotlar,
+        indent=4
+        )
+    maxsulotlar_json.write(maxsulotlar_data)
+
+with open("xaridorlar.json","w") as xaridorlar_json:
+    xaridorlar_data = json.dumps(
+        data.xaridorlar,
+        indent=4,
+        )
+    xaridorlar_json.write(
+        xaridorlar_data
+        )
+
+with open("admins.json","w") as admins_json:
+    admins_data = json.dumps(
+        data.DOKON_EGALARI,
+        indent=4,
+        )
+    admins_json.write(
+        admins_data
+        )
+m_mal = open("maxsulotlar_malumoti.json","w")
+m_mal_data = json.dumps(
+    data.maxsulotlar_malumoti,
+    indent=4,
+    )
+print(m_mal_data, file=m_mal)
+m_mal.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
